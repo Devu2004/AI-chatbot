5ca464e9caa1f6f7752503e250bb13878cfd6453
Devansh <162863042+Devu2004@users.noreply.github.com>
2025-12-23 13:10:06 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,9 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173", // Tera React ka port yahi hona chahiye
+    origin: "http://localhost:5173", 
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true // Cookies aur Tokens ke liye zaruri hai
+    credentials: true 
 }));
 app.use(express.json())
 app.use(cookieparser())
